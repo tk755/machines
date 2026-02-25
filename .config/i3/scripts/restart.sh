cb1f20e4cd8defea3069a6544ed9ea8564deba98
Tushar Khan <dev@tusharkhan.com>
2026-02-24 20:13:29 -0500

    add file extensions to non-PATH scripts

--- .config/i3/scripts/restart.sh
+++ .config/i3/scripts/restart.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # enable/disable laptop keyboard
-$HOME/.config/i3/scripts/laptopkb
+$HOME/.config/i3/scripts/laptopkb.py
 
 # monitor configuration and wallpapers
-$HOME/.config/i3/scripts/setdisplay
+$HOME/.config/i3/scripts/setdisplay.py
 
 # launch polybar
 $HOME/.config/polybar/launch.sh
