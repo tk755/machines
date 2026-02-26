0889e9a13abaec4d9d7ea3e8455ba174e09197d3
Tushar Khan <dev@tusharkhan.com>
2026-02-26 02:51:10 -0500

    fix bash script errors

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CENTER_ICON=$HOME/wbrb.png
-SCREENSHOT=/tmp/lock-screenshot.png
-maim -u $SCREENSHOT
-convert $SCREENSHOT -scale 4% -scale 2500% $SCREENSHOT
-convert $SCREENSHOT $CENTER_ICON -gravity center -composite -matte $SCREENSHOT
-i3lock -u -e -b -i $SCREENSHOT
+CENTER_ICON="$HOME/wbrb.png"
+SCREENSHOT="/tmp/lock-screenshot.png"
+maim -u "$SCREENSHOT"
+convert "$SCREENSHOT" -scale 4% -scale 2500% "$SCREENSHOT"
+convert "$SCREENSHOT" "$CENTER_ICON" -gravity center -composite -matte "$SCREENSHOT"
+i3lock -u -e -b -i "$SCREENSHOT"
