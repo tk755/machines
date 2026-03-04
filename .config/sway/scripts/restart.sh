8b19ff79e71344c6d29cc3f26064604d87c254f7
Tushar Khan <dev@tusharkhan.com>
2026-03-03 22:41:17 -0500

    set sway wallpaper dynamically

--- .config/sway/scripts/restart.sh
+++ .config/sway/scripts/restart.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+# set wallpaper
+wallpaper_dir="$HOME/Dropbox/media/pictures/wallpapers/active"
+if [[ -f "$wallpaper_dir/$(hostname)" ]]; then
+    swaymsg "output * bg '$wallpaper_dir/$(hostname)' fill"
+elif [[ -f "$wallpaper_dir/default" ]]; then
+    swaymsg "output * bg '$wallpaper_dir/default' fill"
+fi
+
 # import gtk3 settings
 source "$HOME/.config/sway/scripts/import-gtk3-settings.sh"
