0d7e2b7c2c7905d935ae400aa6b3e4752bb1c88b
Tushar Khan <dev@tusharkhan.com>
2026-03-06 01:43:04 -0500

    remove ineffective wifi power save workaround

--- .hosts/suzuki/overlays/etc/NetworkManager/dispatcher.d/90-wifi-powersave.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# disable wifi power save to prevent DISASSOC_DUE_TO_INACTIVITY disconnects (MT7921)
-if [[ "$2" == "up" && "$(nmcli -g GENERAL.TYPE device show "$1" 2>/dev/null)" == "wifi" ]]; then
-    iw dev "$1" set power_save off
-fi
