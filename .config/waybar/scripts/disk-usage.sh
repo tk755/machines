0a5d7f531ed2a276e7842ca5f036262fe80f0885
Tushar Khan <dev@tusharkhan.com>
2026-03-08 22:57:17 -0400

    waybar style cleanup

--- .config/waybar/scripts/disk-usage.sh
+++ .config/waybar/scripts/disk-usage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-df --output=used,size / | tail -1 | awk '{
+df --output=used,size / | awk 'NR==2 {
     used = $1 / 1048576
     total = $2 / 1048576
     if (used >= 1000) printf "%.1f TiB", used / 1024
